bf01f167b34d3574d79c548873459ee8f604d235
Vishanth Madugula <vishanth.madugula@gmail.com>
2026-03-11 11:42:59 +0530

    Update Register.jsx

--- src/Register.jsx
+++ src/Register.jsx
@@ -15,7 +15,7 @@ function Register(){
 
   const submit = async () =>{
     try{
-      const res = await axios.post("https://Cabsystemsms-1.onrender/register",data);
+      const res = await axios.post("https://Cab-system-bwfb.onrender/users",data);
       alert(res.data);
     }
     catch(err){
